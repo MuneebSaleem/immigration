71d2f206f1a0ca528ceb3114e9ea95b77f298fe1
MuneebSaleem <35419694+MuneebSaleem@users.noreply.github.com>
2024-02-28 23:57:51 +0500

    remove cdn

--- resources/views/home_custom.blade.php
+++ resources/views/home_custom.blade.php
@@ -421,10 +421,12 @@
 
 
 @endsection
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script src="{{ asset('dist/js/jquery-3.6.4.min.js') }}"></script>
+<script src="{{ asset('dist/js/moment.min.js') }}"></script>
+<script src="{{ asset('dist/js/jquery.min.js') }}"></script>
 
 <script>
     $(document).ready(function(){
